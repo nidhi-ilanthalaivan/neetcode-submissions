8d297ed41ad9787faf53e826efa7ffe96a53a576
nidhi-ilanthalaivan <138754840+nidhi-ilanthalaivan@users.noreply.github.com>
2026-08-10 19:29:35 -0500

    Bulk sync: 12 submissions

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,28 @@
+class MinStack:
+
+    def __init__(self):
+        self.main_stack = []
+        self.second_stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.main_stack.append(val)
+        if not self.second_stack or val <= self.second_stack[-1]:
+            self.second_stack.append(val)
+        
+
+    def pop(self) -> None:
+        if not self.second_stack or self.main_stack[-1] == self.second_stack[-1]:
+            self.main_stack.pop()
+            self.second_stack.pop()
+        else:
+            self.main_stack.pop()
+        
+
+    def top(self) -> int:
+        return self.main_stack.pop()
+        
+
+    def getMin(self) -> int:
+        return self.second_stack.pop()
+        
---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,30 @@
+class MinStack:
+
+    def __init__(self):
+        self.main_stack = []
+        self.second_stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.main_stack.append(val)
+        if not self.second_stack or val <= self.second_stack[-1]:
+            self.second_stack.append(val)
+        
+
+    def pop(self) -> None:
+        if self.second_stack and self.main_stack[-1] == self.second_stack[-1]:
+            self.main_stack.pop()
+            self.second_stack.pop()
+        else:
+            self.main_stack.pop()
+        
+
+    def top(self) -> int:
+        return self.main_stack.pop()
+        
+
+    def getMin(self) -> int:
+        if self.second_stack:
+            return self.second_stack.pop()
+            
+        
